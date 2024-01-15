ee02f8de8720fbe3c7841cb80774971090e4f42b
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-15 19:34:02 +0100

    Revert "python311Packages.azure-mgmt-batch: 17.1.0 -> 17.2.0"
    This reverts commit 33f9b4910fec8bb15e897503f7eb0cd68625f59a.

--- pkgs/development/python-modules/azure-mgmt-batch/default.nix
+++ pkgs/development/python-modules/azure-mgmt-batch/default.nix
@@ -1,46 +1,39 @@
 { lib
-, azure-common
-, azure-mgmt-core
 , buildPythonPackage
 , fetchPypi
-, isodate
+, msrest
+, msrestazure
+, azure-common
+, azure-mgmt-core
 , pythonOlder
-, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "azure-mgmt-batch";
-  version = "17.2.0";
-  pyproject = true;
+  version = "17.1.0";
+  format = "setuptools";
 
-  disabled = pythonOlder "3.9";
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ihXjijfW5OzilXPegIxaiSdsmfJSDqHzUrhqcEyJhY0=";
+    extension = "zip";
+    hash = "sha256-OFv5IImNwtWAeGX75FAZ8QzfvvZRxtv6R0WoQlJduvs=";
   };
 
-  nativeBuildInputs = [
-    setuptools
-  ];
-
   propagatedBuildInputs = [
+    msrest
+    msrestazure
     azure-common
     azure-mgmt-core
-    isodate
   ];
 
-  # Tests are only available in mono repo
+  # Module has no tests
   doCheck = false;
-
-  pythonImportsCheck = [
-    "azure.mgmt.batch"
-  ];
 
   meta = with lib; {
     description = "This is the Microsoft Azure Batch Management Client Library";
-    homepage = "https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/batch/azure-mgmt-batch";
-    changelog = "https://github.com/Azure/azure-sdk-for-python/tree/azure-mgmt-batch_${version}/sdk/batch/azure-mgmt-batch";
+    homepage = "https://github.com/Azure/azure-sdk-for-python";
     license = licenses.mit;
     maintainers = with maintainers; [ maxwilson ];
   };
